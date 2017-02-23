8c4f1890dd7d5580732855c58e47b47aea32376c
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-02-23 19:02:37 +0100

    Base configure & predict & broadcast

--- laser_odometry_core/include/laser_odometry_core/laser_odometry_core.h
+++ laser_odometry_core/include/laser_odometry_core/laser_odometry_core.h
@@ -5,18 +5,22 @@
 #include <geometry_msgs/Pose2D.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <property_bag/property_bag.h>
+#include <tf/tf.h>
+#include <tf/transform_broadcaster.h>
 
 namespace laser_odometry
 {
 
-  class LaserOdometryBase{
-
+  class LaserOdometryBase
+  {
   public:
 
     LaserOdometryBase()          = default;
     virtual ~LaserOdometryBase() = default;
 
-    virtual bool configure(const property_bag::PropertyBag &/*parameters*/) = 0;
+    virtual bool configure(const property_bag::PropertyBag &parameters);
+
+    virtual bool getOrigin();
 
     virtual bool process(const sensor_msgs::LaserScan& /*scan*/,
                          geometry_msgs::Pose2DPtr /*pose*/) = 0;
@@ -31,7 +35,28 @@ namespace laser_odometry
 
   protected:
 
-    bool configured_;
+    bool configured_ = false;
+    bool publish_tf_ = false;
+
+    std::string base_frame_;
+    std::string laser_frame_;
+    std::string world_frame_;
+
+    tf::Transform base_to_laser_; // static, cached
+    tf::Transform laser_to_base_; // static, cached, calculated from base_to_laser_
+
+    tf::Transform relative_tf_;   // last_scan-to-curent_scan tf
+    tf::Transform world_origin_;  // world-origin tf
+    tf::Transform world_to_base_; // world-to-base tf, integrated odom
+
+    sensor_msgs::LaserScan last_scan_;
+
+    ros::Time current_time_;
+
+    tf::TransformBroadcaster tf_broadcaster_;
+
+    virtual tf::Transform predict(const tf::Transform& tf);
+    virtual void broadcastTf();
   };
 
   typedef boost::shared_ptr<LaserOdometryBase> LaserOdometryBasePtr;
--- laser_odometry_core/src/laser_odometry_core.cpp
+++ laser_odometry_core/src/laser_odometry_core.cpp
@@ -1,13 +1,45 @@
 #include <laser_odometry_core/laser_odometry_core.h>
 
-#include <tf/tf.h>
+#include <laser_odometry_core/laser_odometry_utils.h>
 
 namespace laser_odometry
 {
 
-LaserOdometryBase::process(const sensor_msgs::LaserScan& scan,
-                           geometry_msgs::PoseWithCovarianceStampedPtr pose)
+bool LaserOdometryBase::configure(const property_bag::PropertyBag &parameters)
 {
+  parameters.getPropertyValue<std::string>("base_frame",
+                                           base_frame_, "base_frame");
+
+  parameters.getPropertyValue<std::string>("laser_frame",
+                                           laser_frame_, "base_laser_frame");
+
+  parameters.getPropertyValue<std::string>("world_frame",
+                                           world_frame_, "world_frame");
+
+  base_to_laser_.setIdentity();
+  utils::getTf(laser_frame_, base_frame_, base_to_laser_);
+  laser_to_base_ = base_to_laser_.inverse();
+
+  world_to_base_.setIdentity();
+
+  // If set here, derived classes may struggle
+  //configured_ = true;
+
+  return true;
+}
+
+bool LaserOdometryBase::getOrigin()
+{
+  world_origin_.setIdentity();
+
+  return true;
+}
+
+bool LaserOdometryBase::process(const sensor_msgs::LaserScan& scan,
+                                geometry_msgs::PoseWithCovarianceStampedPtr pose)
+{
+  current_time_ = scan.header.stamp;
+
   geometry_msgs::Pose2DPtr pose_2d_ptr = boost::make_shared<geometry_msgs::Pose2D>();
 
   if (pose == nullptr)
@@ -20,15 +52,30 @@ LaserOdometryBase::process(const sensor_msgs::LaserScan& scan,
   pose->pose.pose.position.z = 0;
 
   tf::Quaternion q; q.setRPY(0, 0, pose_2d_ptr->theta);
-
-  pose->pose.pose.orientation = q;
+  tf::quaternionTFToMsg(q, pose->pose.pose.orientation);
 
   // @todo
   //pose->pose.covariance;
 
   //pose->header
 
+  relative_tf_.setIdentity();
+
   return processed;
+}
+
+tf::Transform LaserOdometryBase::predict(const tf::Transform& /*tf*/)
+{
+  tf::Transform tmp_tf; tmp_tf.setIdentity();
+  return tmp_tf;
+}
+
+void LaserOdometryBase::broadcastTf()
+{
+  tf::StampedTransform transform_msg(world_to_base_, current_time_,
+                                     world_frame_, base_frame_);
+
+  tf_broadcaster_.sendTransform(transform_msg);
 }
 
 } /* namespace laser_odometry */
