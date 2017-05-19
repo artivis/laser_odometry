d6a818c67d2cd547d649a070a7afb479de0caf53
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-05-19 13:13:27 +0200

    base doc

--- laser_odometry_core/include/laser_odometry_core/laser_odometry_base.h
+++ laser_odometry_core/include/laser_odometry_core/laser_odometry_base.h
@@ -16,6 +16,10 @@
 
 namespace laser_odometry
 {
+  /**
+   * @enum OdomType
+   * @brief The type of odometry computed.
+   */
   enum class OdomType : std::size_t
   {
     Unknown,
@@ -25,31 +29,122 @@ namespace laser_odometry
     Odom3DCov
   };
 
+  /**
+   * @class LaserOdometryBase.
+   * @brief Base class for the laser_odometry plugins.
+   *
+   * This class represents an abstract scan/pointcloud matcher for
+   * odometry computation.
+   *
+   * It is though as a key-frame based scan/pointcloud matcher,
+   * meaning that the reference reading is only updated upon
+   * isKeyFrame() function returning true.
+   *
+   * The derived class is required to implement a limited number
+   * of functions (see below) while most of the common operations are implemented
+   * in this base class (e.g. frame composition).
+   *
+   *
+   * Given a new reading, the overall algorithm takes place in LaserOdometryBase::process
+   * and can be summarized as follows:
+   *
+   * process :
+   *
+   *       - if first reading
+   *           - initialize                         [O]
+   *           - return
+   *
+   *       - preProcessing                          [O]
+   *
+   *       - predict                                [O]
+   *
+   *       - process_impl                           [M]
+   *
+   *       - is_keyframe = isKeyFrame               [O]
+   *
+   *       - if is_keyframe is true
+   *
+   *          - isKeyFrame                          [O]
+   *
+   *       - if is_keyframe is \b not true
+   *
+   *          - isNotKeyFrame                       [O]
+   *
+   *       - postProcessing                         [O]
+   *
+   * [M] Symbol marks the necessity for the base class to override the function.<br>
+   * [O] Symbol marks optionality for the base class to override the function.
+   *
+   */
   class LaserOdometryBase
   {
   public:
 
     using covariance_t = geometry_msgs::PoseWithCovariance::_covariance_type;
 
+    /// @brief A brief report of the matching.
     struct ProcessReport;
 
   public:
 
+    /**
+     * @brief Default constructor.
+     */
     LaserOdometryBase()          = default;
+
+    /**
+     * @brief Default destructor.
+     */
     virtual ~LaserOdometryBase() = default;
 
-    ProcessReport process(const sensor_msgs::LaserScanConstPtr& cloud_msg,
+    /**
+     * @brief Compute the 2D odometry given a LaserScan.
+     * @param[in] scan_msg. The input LaserScan.
+     * @param[out] pose_msg. The estimated 2D pose.
+     * @param[out] relative_pose_msg. The estimated 2D pose increment.
+     * @return ProcessReport. A brief summary of the scan matching process.
+     *
+     * @see ProcessReport
+     */
+    ProcessReport process(const sensor_msgs::LaserScanConstPtr& scan_msg,
                           geometry_msgs::Pose2DPtr pose_msg,
                           geometry_msgs::Pose2DPtr relative_pose_msg = nullptr);
 
-    ProcessReport process(const sensor_msgs::LaserScanConstPtr& cloud_msg,
+    /**
+     * @brief Compute the 3D (actually 2D) odometry given a LaserScan.
+     * @param[in] scan_msg. The input LaserScan.
+     * @param[out] pose_msg. The estimated 3D odometry (actually 2D).
+     * @param[out] relative_odom_msg. The estimated 3D odometry increment. @note This will probably disapear.
+     * @return ProcessReport. A brief summary of the scan matching process.
+     *
+     * @see ProcessReport
+     */
+    ProcessReport process(const sensor_msgs::LaserScanConstPtr& scan_msg,
                           nav_msgs::OdometryPtr pose_msg,
                           nav_msgs::OdometryPtr relative_odom_msg = nullptr);
 
+    /**
+     * @brief Compute the 2D odometry given a PointCloud2.
+     * @param[in] cloud_msg. The input PointCloud2.
+     * @param[out] pose_msg. The estimated 2D pose.
+     * @param[out] relative_pose_msg. The estimated 2D pose increment.
+     * @return ProcessReport. A brief summary of the pointcloud matching process.
+     *
+     * @see ProcessReport
+     */
     ProcessReport process(const sensor_msgs::PointCloud2ConstPtr& cloud_msg,
                           geometry_msgs::Pose2DPtr pose_msg,
                           geometry_msgs::Pose2DPtr relative_pose_msg = nullptr);
 
+    /**
+     * @brief Compute the 3D odometry given a \c PointCloud2.
+     * @param[in] cloud_msg. The input \c PointCloud2.
+     * @param[out] pose_msg. The estimated 3D odometry.
+     * @param[out] relative_odom_msg. The estimated 3D odometry increment. @note This will probably disapear.
+     * @return ProcessReport. A brief summary of the pointcloud matching process.
+     *
+     * @see ProcessReport
+     */
     ProcessReport process(const sensor_msgs::PointCloud2ConstPtr& cloud_msg,
                           nav_msgs::OdometryPtr pose_msg,
                           nav_msgs::OdometryPtr relative_odom_msg = nullptr);
@@ -58,72 +153,167 @@ namespace laser_odometry
 
     /* Those are the functions the derived class should overload */
 
+    /**
+     * @brief Function to be implemented by the derived plugin.
+     * It is the core (implementation) of the LaserOdometryBase::process function.
+     * The base class implementation throws a std::runtime_error in case the function
+     * is called while not being overloaded.
+     * @param[in] laser_msg. The input LaserScan.
+     * @param[in] prediction. A prediction regarding the displacement increment.
+     * @return Whether or not the scan matching was successful.
+     *
+     * @note [Necessary] To be implemented in the derived class.
+     */
     virtual bool process_impl(const sensor_msgs::LaserScanConstPtr& laser_msg,
                               const tf::Transform& prediction);
 
+    /**
+     * @brief Function to be implemented by the derived plugin.
+     * It is the core (implementation) of the LaserOdometryBase::process function.
+     * The base class implementation throws a std::runtime_error in case the function
+     * is called while not being overloaded.
+     * @param[in] cloud_msg. The input PointCloud2.
+     * @param[in] prediction. A prediction regarding the displacement increment.
+     * @return Whether or not the pointcloud matching was successful.
+     *
+     * @note [Necessary] To be implemented in the derived class.
+     */
     virtual bool process_impl(const sensor_msgs::PointCloud2ConstPtr& cloud_msg,
                               const tf::Transform& prediction);
 
   public:
 
+    /**
+     * @brief Return the current estimated pose.
+     * @return A const-reference tf::Transform.
+     */
     const tf::Transform& getEstimatedPose() const noexcept;
 
+    /**
+     * @brief Reset the matcher.
+     * The base class implemetation resets all transforms to
+     * Identity and the key-reading to nullptr.
+     */
     virtual void reset();
 
+    /**
+     * @brief Configure the base class various parameters.
+     * It internally calls configureImpl() to allow
+     * the derived class to have it own configuration.
+     * @return Whether the class is configured or not.
+     *
+     * @see configureImpl.
+     */
     bool configure();
 
+    /**
+     * @return Whether the class is configured or not.
+     */
     bool configured() const noexcept;
 
     /* Guetter / Setter */
 
+    /// \return Reference to the origin frame.
     tf::Transform& getOrigin();
+    /// \return Const-reference to the origin frame.
     const tf::Transform& getOrigin() const;
 
+    /**
+     * @brief Set the origin frame.
+     * @param[in] origin. The origin frame.
+     */
     void setOrigin(const tf::Transform& origin);
 
+    /// \return Reference to the initial prediction transform of the upcoming matching.
     tf::Transform& getInitialGuess();
+    /// \return Const-reference to the initial prediction transform of the upcoming matching.
     const tf::Transform& getInitialGuess() const;
 
+    /**
+     * @brief Set the initial prediction of the upcoming matching.
+     * @param[in] guess. The initial prediciton.
+     */
     void setInitialGuess(const tf::Transform& guess);
 
+    /// \return Reference to the laser pose wrt the robot base frame.
     tf::Transform& getLaserPose();
+    /// \return Const-reference to the laser pose wrt the robot base frame.
     const tf::Transform& getLaserPose() const;
 
+    /**
+     * @brief Set the laser pose wrt the robot base frame.
+     * @param[in] base_to_laser.
+     */
     void setLaserPose(const tf::Transform& base_to_laser);
 
+    /// \return the robot base frame name.
     const std::string& getFrameBase()  const noexcept;
+    /// \return the robot laser frame name.
     const std::string& getFrameLaser() const noexcept;
+    /// \return the global fixed frame name.
     const std::string& getFrameFixed() const noexcept;
+    /// \return the global odometry frame name.
     const std::string& getFrameOdom()  const noexcept;
 
+    /// @brief Set the robot base frame name.
+    /// @param[in] frame.
     void setFrameBase(const std::string& frame);
+    /// @brief Set the robot laser frame name.
+    /// @param[in] frame.
     void setFrameLaser(const std::string& frame);
+    /// @brief Set the global fixed frame name.
+    /// @param[in] frame.
     void setFrameFixed(const std::string& frame);
+    /// @brief Set the global odometry frame name.
+    /// @param[in] frame.
     void setFrameOdom(const std::string& frame);
 
+    /**
+     * @brief Get the time stamp of the latest reading evaluated.
+     * @return Const-reference ros::Time.
+     */
     const ros::Time& getCurrentTime() const noexcept;
 
+    /// @return The type of odometry computed by the plugin.
+    /// If not overrided, default is OdomType::Unknown
+    /// @see OdomType
     virtual OdomType odomType() const noexcept;
 
+    /**
+     * @return Whether the reference reading has been updated or not.
+     */
     bool hasNewKeyFrame() const noexcept;
 
+    /**
+     * @brief Get the referent LaserScan.
+     * @param[in]
+     */
     void getKeyFrame(sensor_msgs::LaserScanConstPtr& kframe) const noexcept;
+
+    /**
+     * @brief Get the referent PointCloud2.
+     * @param[in]
+     */
     void getKeyFrame(sensor_msgs::PointCloud2ConstPtr& kframe) const noexcept;
 
   protected:
 
-    bool configured_   = false;
-    bool initialized_  = false;
-    bool broadcast_tf_ = false;
-    bool has_new_kf_   = false;
+    bool configured_   = false; /*!< Whether the matcher is configured. */
+    bool initialized_  = false; /*!< Whether the matcher is initialized. */
+    bool broadcast_tf_ = false; /*!< Whether the matcher should publish on tf. */
+    bool has_new_kf_   = false; /*!< Whether the matcher has a new referent reading. */
 
-    covariance_t covariance_;
+    covariance_t covariance_;  /*!< The estimated pose covariance. */
+
 
     ros::NodeHandle private_nh_ = ros::NodeHandle("~");
 
-    std::string base_frame_       = "base_link";
-    std::string laser_frame_      = "base_laser_link";
-    std::string world_frame_      = "map";
+    std::string base_frame_       = "base_link";        /*!< The robot base frame name. */
+    std::string laser_frame_      = "base_laser_link";  /*!< The robot laser frame name. */
+    std::string world_frame_      = "map";              /*!< The global fixed frame name. */
+
+    /// @brief The global odometry frame name.
+    /// This frame name is only used in the published message.
     std::string laser_odom_frame_ = "odom";
 
     /// \brief base_to_laser_
@@ -179,7 +369,10 @@ namespace laser_odometry
     /// == world_origin_ * world_to_base_
     tf::Transform world_origin_to_base_;
 
+    /// \brief  The referent LaserScan.
     sensor_msgs::LaserScanConstPtr   reference_scan_;
+
+    /// \brief  The referent PointCloud2.
     sensor_msgs::PointCloud2ConstPtr reference_cloud_;
 
     /// \brief current_time_
@@ -187,28 +380,91 @@ namespace laser_odometry
     /// to the last processed message.
     ros::Time current_time_;
 
+    /**
+     * @brief Called within configure(), it allow sthe derived class
+     * to perform its configuration step.
+     * @return Whether the class is configured or not.
+     *
+     * @note The base class returns \b true by default.
+     */
     virtual bool configureImpl();
 
+    /**
+     * @brief Call upon the first LaserScan reception it allows the derived class
+     * to perfom some reading-dependent initializations.
+     * @param[in] The first LaserScan reading.
+     * @return Whether the class is initialized.
+     *
+     * @note The base class returns \b true by default.
+     */
     virtual bool initialize(const sensor_msgs::LaserScanConstPtr&   scan_msg);
+
+    /**
+     * @brief Call upon the first PointCloud2 reception it allows the derived class
+     * to perfom some reading-dependent initializations.
+     * @param[in] The first PointCloud2 reading.
+     * @return Whether the class is initialized.
+     *
+     * @note The base class returns \b true by default.
+     */
     virtual bool initialize(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
 
+    /**
+     * @brief Perform a prediction of the pose increment for the upcoming matching.
+     * @param[in] The last estimated pose increment in the \b world_frame_ frame.
+     * @return A prediction of the upcoming pose increment.
+     *
+     * @note The base class returns tf::Transform::getIdentity() by default.
+     */
     virtual tf::Transform predict(const tf::Transform& tf);
 
+    /**
+     * @brief Allows the derived class to perform some pre-processing
+     * before the actual matching.
+     *
+     * @note The base class does nothing by default.
+     */
     virtual void preProcessing();
 
+    /**
+     * @brief Allows the derived class to perform some post-processing
+     * after the actual matching.
+     *
+     * @note The base class does nothing by default.
+     */
     virtual void postProcessing();
 
+    /**
+     * @brief Evaluate if the current reading should become the new
+     * referent reading form the matching.
+     * @param[in] The estimated pose increment in the world_frame_ frame.
+     * @return Whether the currently evaluated reading is the new referent.
+     *
+     * @note The base class returns \b true by default.
+     */
     virtual bool isKeyFrame(const tf::Transform& tf);
 
+    /**
+     * @brief Allows the derived class to perform some actions if the
+     * currently evaluated reading is the new referent.
+     */
     virtual void isKeyFrame();
-    virtual void isNotKeyFrame();
 
+    /**
+     * @brief Allows the derived class to perform some actions if the
+     * currently evaluated reading is \b NOT the new referent.
+     */
+    virtual void isNotKeyFrame();
     virtual tf::Transform expressFromLaserToBase(const tf::Transform& tf_in_lf);
 
+    /**
+     * @brief Fills the published message with the estimated pose increment.
+     */
     template <typename T>
     void fillMsg(T& msg_ptr);
   };
 
+  /// @brief A base-class pointer.
   typedef boost::shared_ptr<LaserOdometryBase> LaserOdometryPtr;
 
   template <>
