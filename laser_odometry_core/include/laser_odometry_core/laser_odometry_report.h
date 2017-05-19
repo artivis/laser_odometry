cef113cfeedd665823232858010d901d770e58b3
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-05-19 13:33:27 +0200

    ProcessReport doc

--- laser_odometry_core/include/laser_odometry_core/laser_odometry_report.h
+++ laser_odometry_core/include/laser_odometry_core/laser_odometry_report.h
@@ -6,20 +6,45 @@
 namespace laser_odometry
 {
 
+  /**
+   * @brief A (very)-simple report of the scan matching process.
+   */
   struct LaserOdometryBase::ProcessReport
   {
     ProcessReport() = default;
+
+    /**
+     * @brief Constructor given one \c bool arg.
+     * @param[in] p. Whether the matching was successful or not.
+     */
     explicit ProcessReport(bool p) : processed(p) { }
+
+    /**
+     * @brief Constructor given two \c bool args.
+     * @param[in] p. Whether the matching was successful or not.
+     * @param[in] k. evaluated reading has become the new referent reading.
+     */
     explicit ProcessReport(bool p, bool k) : processed(p), key_frame(k) { }
 
+    /// @brief Whether the matching was successful or not.
     const bool processed = false;
+
+    /// @brief Whether the evaluated reading has become the new
+    /// referent reading.
     const bool key_frame = true;
 
+    /**
+     * @brief Conversion to \c bool operator.
+     * @return Whether the matching was successful or not.
+     */
     bool operator()() const noexcept
     {
       return processed;
     }
 
+    /**
+     * @brief A default error report.
+     */
     static ProcessReport ErrorReport()
     {
       static const ProcessReport error_report{false, false};
